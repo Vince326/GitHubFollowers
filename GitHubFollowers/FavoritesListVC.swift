a533d79ae62f36f2347acc63a797ab40fdbe863c
Vincent Hunter <jhearst29@gmail.com>
2020-11-17 18:29:13 -0500

    Created New VC's and Search UI
    Created the new search VC UI programmatically. Created the properties that the GF Button and GF Textfield will use throughout application. Also configured the logo, imageview and actionButton for the search page of the application. Set constraints programmatically

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Vincent Hunter on 11/17/20.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+    }
+    
+
+   
+
+}
---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Vincent Hunter on 11/17/20.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+   override init(frame: CGRect) {
+    super.init(frame: frame)
+   configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(background:UIColor, title:String){
+        super.init(frame: CGRect.zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
